e25491201621ad0cc491d702ebd078c1a9727783
hoijoii <oodh3328@gmail.com>
2023-06-21 16:06:26 +0900

    test commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="home">
-    
+  <div id="container" class="home">
+    <div class="content">
+      <div class="search">
+        search box
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue';
-//import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
 
 
 </script>
