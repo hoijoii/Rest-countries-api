3db6d023fda4692e472de084cae3d81734404844
hoijoii <oodh3328@gmail.com>
2023-06-21 13:57:55 +0900

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+          @import '@/assets/scss/default.scss';
+          @import '@/assets/scss/commons.scss';
+        `
+      }
+    }
+  },
 })
