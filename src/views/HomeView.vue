4545ece875e6e7d507d9189182bdaa781815c1bc
hoijoii <oodh3328@gmail.com>
2023-06-21 17:56:38 +0900

    Feat: filter

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,45 @@
 <template>
   <div id="container" class="home">
     <div class="content">
-      <div class="search">
-        search box
+      <div class="filter">
+        <div class="search-box">
+          <input type="text" class="search filter-design" placeholder="Search for a country..."/>
+          <ion-icon name="search" />
+        </div>
+        <div class="region">
+          <select v-model="region" required class="slct filter-design">
+            <option value="" hidden selected>Filter by Region</option>
+            <option v-for="(re, idx) in regions" :key="idx" :value="re">{{ re }}</option>
+          </select>
+        </div>
       </div>
+      
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
+/* eslint-disable */
+import { onMounted, reactive, Ref, ref } from 'vue';
 
+const region : Ref<string> = ref('')
 
+// filter by region
+const data = require('@/assets/data/data.json')
+const regions : Array<string> = reactive(['Filter by Region'])
+
+const setRegions = ():void => {
+  data.forEach((country:any) => {
+    if(!regions.includes(country.region)) {
+      regions.push(country.region)
+    }
+  });
+  regions.sort()
+}
+
+// lifecycle
+onMounted(()=>{
+  setRegions()
+})
 
 </script>
